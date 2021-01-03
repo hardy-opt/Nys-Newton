396e1a057715600cc7a8deb6a5c04ee347b44842
hardy-opt <hardik_07@outlook.com>
2021-01-03 10:23:09 +0900

    one file

---
+++ Run_me_first.m
@@ -0,0 +1,109 @@
+% Add folders to path.
+
+addpath(pwd);
+
+cd Stochastic_solver/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Emp_risk_problem/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Optimum/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Data/;
+addpath(genpath(pwd));
+%cd ..;
+
+cd Synthetic_data_generator/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Classification/;
+addpath(genpath(pwd));
+%cd ..;
+
+cd Gisette_Data/;
+addpath(genpath(pwd));
+cd ..;
+
+%cd data/A
+cd Arcene_Data/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Madelon_Data/;
+addpath(genpath(pwd));
+cd ..;
+
+
+cd rcv1.binary/;
+addpath(genpath(pwd));
+cd ..;
+
+cd icnn1/;
+addpath(genpath(pwd));
+cd ..;
+
+cd w8a/;
+addpath(genpath(pwd));
+cd ..;
+
+cd MNIST/;
+addpath(genpath(pwd));
+cd ..;
+cd cover_type.binary/;
+addpath(genpath(pwd));
+cd ..;
+
+cd ..;
+cd Regression/;
+addpath(genpath(pwd));
+%cd ..;
+
+cd Airfoil/
+addpath(genpath(pwd));
+cd ..;
+
+cd Combined_Cycle_power_plant/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Forest_fires/;
+addpath(genpath(pwd));
+cd ..;
+
+cd UJIndoorLoc/;
+addpath(genpath(pwd));
+cd ..;
+
+cd YearPMSD/;
+addpath(genpath(pwd));
+cd ..;
+
+cd Slice/;
+addpath(genpath(pwd));
+cd ..;
+
+
+cd Bodyfat/;
+addpath(genpath(pwd));
+cd ..;
+
+
+cd Abalone/;
+addpath(genpath(pwd));
+cd ..;
+
+
+cd E2006_tfidf/;
+addpath(genpath(pwd));
+cd ..;
+
+
+cd ..
+cd ..
+fprintf(' Welcome to Stochastic gradient library to solve EMP risk minimization problem! \n');
